3a68fab52c5065ac1097b8a43d0be2c5a09dd9a5
Aleksandr Tumanov <tumalexx02@gmail.com>
2025-03-03 11:40:04 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Typography } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <Typography variant="h2" component="h2" fontWeight={'medium'}>
+        Notes App
+      </Typography>
+    </>
+  )
+}
+
+export default App
